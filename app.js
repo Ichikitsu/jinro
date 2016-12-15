070806621e06c5848400eb368a7e96cf68f17bb0
吉津市 <Ichi@MacBook-Air-1.local>
2016-12-16 08:01:53 +0900

    init

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var http = require("http");
 var socketio = require("socket.io");
-//var fs = require("fs");
+var fs = require("fs");
 
 //var server = http.createServer(function(req, res) {
      //res.writeHead(200, {"Content-Type":"text/html"});
