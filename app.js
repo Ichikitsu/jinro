79b0bedfa0d075ae64e3e35b7718bd474d7f3b99
吉津市 <Ichi@MacBook-Air-1.local>
2016-12-16 08:14:45 +0900

    init

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var http = require("http");
 var socketio = require("socket.io");
 var fs = require("fs");
-//ver
+//v
 var server = http.createServer(function(req, res) {
      res.writeHead(200, {"Content-Type":"text/html"});
      var output = fs.readFileSync("./index.html", "utf-8");
